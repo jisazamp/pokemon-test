8b6f6da8796ca8fb3bdeb16127b009eedb9a9b4f
Juan P. Isaza <jisazamp@gmail.com>
2022-07-20 14:51:00 -0500

    Finished layout for desktop

--- src/components/PokemonForm.jsx
+++ src/components/PokemonForm.jsx
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FaTimes, FaSave } from 'react-icons/fa';
 
 import Button from '../common/Button';
 
 const PokemonForm = ({ setFormVisible }) => {
+  const [pokemon, setPokemon] = useState({
+    name: '',
+    image: '',
+    attack: '10',
+    defense: '10',
+  });
+
+  const { name, image, attack, defense } = pokemon;
+
+  const handleInputChange = (e) => {
+    setPokemon({ ...pokemon, [e.target.id]: e.target.value });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('submitted');
@@ -15,24 +29,66 @@ const PokemonForm = ({ setFormVisible }) => {
       <form onSubmit={handleSubmit} className='new-pokemon'>
         <p>Nuevo Pokemon</p>
 
-        <div>
-          <label htmlFor='name'></label>
-          <input id='name' type='text' placeholder='Nombre' />
+        <div className='input-group'>
+          <label htmlFor='name'>Nombre</label>
+          <input
+            value={name}
+            onChange={handleInputChange}
+            id='name'
+            type='text'
+            placeholder='Nombre'
+          />
         </div>
 
-        <div>
-          <label htmlFor='image'></label>
-          <input id='image' type='text' placeholder='url' />
+        <div id='image' className='input-group'>
+          <label htmlFor='image'>Imagen</label>
+          <input
+            value={image}
+            onChange={handleInputChange}
+            id='image'
+            type='text'
+            placeholder='url'
+          />
         </div>
 
-        <div>
-          <label htmlFor='attack'></label>
-          <input type='range' min='0' max='100' id='attack' />
+        <div className='input-group'>
+          <label htmlFor='attack'>
+            Ataque <span>{attack}</span>
+          </label>
+          <div className='range-group'>
+            <span>0</span>
+            <input
+              value={attack}
+              onChange={handleInputChange}
+              className='slider'
+              type='range'
+              min='0'
+              max='100'
+              step='1'
+              id='attack'
+            />
+            <span>100</span>
+          </div>
         </div>
 
-        <div>
-          <label htmlFor='defense'></label>
-          <input type='range' min='0' max='100' id='defense' />
+        <div className='input-group'>
+          <label htmlFor='defense'>
+            Defensa <span>{defense}</span>
+          </label>
+          <div className='range-group'>
+            <span>0</span>
+            <input
+              value={defense}
+              onChange={handleInputChange}
+              className='slider'
+              type='range'
+              min='0'
+              max='100'
+              step='1'
+              id='defense'
+            />
+            <span>100</span>
+          </div>
         </div>
 
         <div className='btn-group'>
